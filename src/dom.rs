c69a003521c014aa1ebe14b6acfe2cc3280522d0
bo <bohuang1994@gmail.com>
2024-09-10 18:14:06 +0800

    post-01: dom

---
+++ src/dom.rs
@@ -0,0 +1,35 @@
+type AttrMap = HashMap<String, String>;
+
+
+struct Node {
+    // data common to all nodes
+    children: Vec<Node>,
+
+    // data specific to each node type
+    node_type: NodeType,
+}
+
+enum NodeType {
+    Text(String),
+    Element(ElementData)
+}
+
+struct ElementData {
+    tag_name: String,
+    attributes: AttrMap,
+}
+
+
+fn text(data: String) -> Node {
+    Node { children: Vec::new(), node_type: NodeType::Text(data) }
+}
+
+pub fn elem(tag_name: String, attrs: AttrMap, children: Vec<Node>) -> Node {
+    Node {
+        children,
+        node_type: NodeType::Element(ElementData {
+            tag_name,
+            attrs,
+        }),
+    }
+}
